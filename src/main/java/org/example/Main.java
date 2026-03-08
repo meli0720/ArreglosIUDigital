175f57dead4704f8a493378b52f5ae3ab18e0879
meli0720 <melimenesesacevedo@gmail.com>
2026-03-07 21:34:30 -0500

    Avances del ejercicio 1 hasta promedio total

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+    public static double sumTotal;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] vector1 = new int[7];
+        int[] vector2 = new int[7];
+
+        Scanner scan = new Scanner(System.in);
+        vector1 = ingresarDatos(scan, vector1);
+        vector2 = ingresarDatos(scan, vector2);
+
+
+
+        int[] vector3 = new int[7];
+
+        //2. Llenar arreglo 3 con la diferencia de lso valores de los otros 2 vectores
+        for (int i = 0; i < vector3.length ; i++) {
+            vector3[i] = vector1[i]-vector2[i];
+            sumTotal += vector3[i];
+            System.out.println("Vector1["+i+"]"+"="+vector1[i]+" - Vector2["+i+"]"+"="+vector2[i]+"     |      Resultado ["+vector3[i]+"]");
+        }
+
+        //3. promedio de todos los datos
+        System.out.println(sumTotal+" / 21 = "+sumTotal/21);
+
+
+
+    }
+
+    public static int[] ingresarDatos(Scanner scan, int[] vector){
+        System.out.println("Ingresa los datos del arreglo:");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Ingrese el valor para la posición: ["+i+"]");
+            vector[i] = scan.nextInt();
+            sumTotal += vector[i];
+        }
+        return vector;
     }
 }
